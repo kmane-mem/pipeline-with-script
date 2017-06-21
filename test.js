2208fb27fa601aecbe991d7a96fffaa24a25b5be
shiptest-rc-me <shiptest.rc.me@gmail.com>
2017-06-21 18:18:09 +0000

    test.js created online with Bitbucket

---
+++ test.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "test sample" : function (browser) {
+    browser
+      .url("http://127.0.0.1:5001")
+      .waitForElementVisible('body', 1000)
+      .assert.containsText('body', 'Hello Woorld')
+      .end();
+  }
+};
